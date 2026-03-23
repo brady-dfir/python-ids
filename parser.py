b5ff59a6d96fa2cc4b28de93047a4b3a3fb28470
Brady <brdypr@gmail.com>
2026-03-23 13:42:44 -0500

    commit

--- parser.py
+++ parser.py
@@ -1,1 +1,13 @@
 # Extracts data
+
+from scapy.all import IP, TCP, UDP, DNS, DNSQR
+
+def parse_packet(pkt):
+    data = {
+        "src": None,
+        "dst": None,
+        "protocol": None,
+        "dport": None,
+        "flags": None,
+        "dns_query": None
+    }
