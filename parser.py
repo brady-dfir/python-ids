b1b0b1d5b1793fba86adb2673b578ddd6b5c642e
Brady <brdypr@gmail.com>
2026-03-24 18:49:39 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -11,3 +11,19 @@ def parse_packet(pkt):
         "flags": None,
         "dns_query": None
     }
+
+    if IP in pkt:
+        data["src"] = pkt[IP].src
+        data["dst"] = pkt[IP].dst
+    if TCP in pkt:
+        data["protocol"] = "TCP"
+        data["dport"] = pkt[TCP].dport
+        data["flags"] = pkt[TCP].flags
+    elif UDP in pkt:
+        data["protocol"] = "UDP"
+        data["dport"] = pkt[UDP].dport
+    if DNS in pkt and pkt[DNS].qd is not None:
+        q = pkt[DNS].qd
+        if isinstance(q, DNSQR):
+            data["dns_query"] = q.qname.decode(errors="ignore").rstrip(".")
+    return data
