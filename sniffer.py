fc87541d497fb66fccc6ed1bd9a10f1046d7947e
Brady <brdypr@gmail.com>
2026-03-31 20:20:17 -0500

    Update sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -13,11 +13,19 @@ from config import INTERFACE
 
 def handle_packet(pkt):
     data = parse_packet(pkt)
-
     src = data["src"]
     dst = data["dst"]
     proto = data["protocol"]
     dport = data["dport"]
     flags = data["flags"]
     dns_query = data["dns_query"]
+
+    if not src or not dst:
+        return
+    detect_malicious_ip(src, dst)
+    if proto == "TCP" and dport:
+        detect_port_scan(src, dst, dport)
+        detect_syn_flood(src, flags)
+    if dns_query:
+        detect_suspicious_dns(src, dns_query)
 
