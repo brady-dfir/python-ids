22ee73393a918c9ec4709328e16313023e473f32
Brady <brdypr@gmail.com>
2026-03-31 20:19:10 -0500

    Update sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -1,1 +1,23 @@
 # Captures packets
+
+from scapy.all import sniff
+from parser import parse_packet
+from detect import (
+    detect_malicious_ip,
+    detect_port_scan,
+    detect_syn_flood,
+    detect_suspic_dns,
+)
+
+from config import INTERFACE
+
+def handle_packet(pkt):
+    data = parse_packet(pkt)
+
+    src = data["src"]
+    dst = data["dst"]
+    proto = data["protocol"]
+    dport = data["dport"]
+    flags = data["flags"]
+    dns_query = data["dns_query"]
+
